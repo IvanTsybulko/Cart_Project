4f71c672187f48a9e05b73a39b4d5ee14107774b
IvanTsybulko <107126601+IvanTsybulko@users.noreply.github.com>
2023-10-10 17:48:17 +0300

    fourth commit

---
+++ src/cart.js
@@ -0,0 +1,107 @@
+let labelElement = document.getElementById('label')
+let shoppingCartElement = document.getElementById('shopping-cart');
+
+let basket = JSON.parse(localStorage.getItem("cart-data")) || [];
+
+let calculation = () => {
+  let cartAmountElement = document.getElementById("cart-amount");
+
+  let sum = 0;
+  for(let i = basket.length; i > 0; i--)
+  {
+    sum += basket[i - 1].item;
+  }
+
+  cartAmountElement.innerHTML = sum;
+}
+
+calculation();
+
+let generateCartItems = () => {
+  if(basket.length !== 0){
+    return (shoppingCartElement.innerHTML = basket.map((x)=>{
+
+      let{id,item} = x;
+      let search = shopItemsData.find((y)=>y.id === id) || [];
+      return `
+      <div class="cart-item">
+      <img width="100" src=${search.img} alt=""/>
+      <div class="details">
+        <div class="name-price-x">
+
+          <h4 class="name">${search.name}</h4>
+          <h4 class="price">${search.price}$</h4>
+          <i class="bi bi-x-lg"></i>
+
+        </div>
+        <div class="buttons">
+            <i onclick="decrement(${id})" class="bi bi-dash-circle"></i>
+            <div id="${id}" class="quantity">${item}</div>
+            <i onclick="increment(${id})" class="bi bi-plus-circle"></i>
+        </div>
+
+        <h3>$ ${item * search.price}</h3>
+      </div>
+    </div>
+      `
+    }).join(" "))
+  }
+  else{
+    shoppingCartElement.innerHTML = `<h2 class="empty">Cart Is Empty</h2>
+    <a href="index.html"><button class="back-home">Back to Home</button></a>`
+  }
+}
+
+generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = document.getElementById(id);
+  
+  let search = basket.find((x)=> x.id === selectedItem.id);
+
+  if(search === undefined)
+  {
+    basket.push(
+      {
+        id: selectedItem.id,
+        item: 1
+      }
+    );
+
+    search = basket.find((x)=> x.id === selectedItem.id);
+  }
+  else{
+    search.item +=1;
+  } 
+
+  update(id, search.item);
+
+  generateCartItems();
+};
+
+let decrement = (id) => {
+  let selectedItem = document.getElementById(id);
+  
+  let search = basket.find((x)=> x.id === selectedItem.id);
+
+  if(search != undefined && search.item > 0)
+  {
+    search.item -=1;
+
+    basket = basket.filter((x)=>x.item !== 0);
+
+    update(id, search.item);
+
+    generateCartItems();
+  }
+};
+
+let update = (id,quantity) => {
+  let selectedItem = document.getElementById(id);
+
+  selectedItem.innerHTML = quantity;
+
+  calculation();
+
+  localStorage.setItem("cart-data", JSON.stringify(basket));
+};
---
+++ src/data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "1",
+    name: "G-Shock MTG1",
+    price: "200",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image1.png"
+  },
+  {
+    id: "2",
+    name: "G-Shock MTG2",
+    price: "250",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image2.png"
+},
+  {
+    id: "3",
+    name: "G-Shock MTG3",
+    price: "275",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image3.png"
+},
+  {
+    id: "4",
+    name: "G-Shock MTG4",
+    price: "400",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/image4.png"
+}
+];
